efd2d9c0b980f53ed15006fdd032cba116264ba9
fotcorn <fotcorn@gmail.com>
2023-12-30 11:42:10 +0000

    use 4 words at end of sentence to detect failed generation

--- sentence_tts.py
+++ sentence_tts.py
@@ -40,7 +40,9 @@ class SentenceTTS:
 
     def _inference(self, text):
         text = text.strip()
-        padded_text = text + " pause"
+        end_of_sentence_text = " end of sentence pause"
+        end_of_sentence_words = [s for s in end_of_sentence_text.split(" ") if s]
+        padded_text = text + end_of_sentence_text
 
         out = self.model.inference(
             padded_text,
@@ -58,39 +60,29 @@ class SentenceTTS:
             self.whisper_model, audio, language="en", seed=None
         )
 
-        last_word = result["segments"][-1]["words"][-1]
-        last_word_text = last_word["text"].lower()
-
         def normalize_word(word):
             word = word.lower()
             word = word.replace(".", "").replace(",", "").replace(";", "")
             word = word.strip()
             return word
 
-        if not "pause" in normalize_word(last_word_text):
-            if normalize_word(text.rsplit(" ", 1)[1]) == normalize_word(last_word_text):
-                print("did not find pause, but found last word")
-                end_sample = int(last_word["end"] * 24000)
-            else:
-                import IPython
+        num_words = len(end_of_sentence_words)
+        last_generated_words = result["segments"][-1]["words"][-num_words:]
+        # Check all words except the last, this one might be garbled.
+        for expected_word, actual_word in zip(
+            end_of_sentence_words[:-1], last_generated_words[:-1]
+        ):
+            if expected_word not in normalize_word(actual_word["text"]):
+                raise Exception(f"Expected word {expected_word} not in {actual_word}")
 
-                IPython.display.display(
-                    IPython.display.Audio(
-                        torch.tensor(out["wav"]).unsqueeze(0), rate=24000
-                    )
-                )
-                print(last_word)
-                print("-------------")
-                print(padded_text)
-                print("-------------")
-                print(
-                    json.dumps(
-                        result["segments"][-1]["words"], indent=2, ensure_ascii=False
-                    )
-                )
-                raise Exception("'pause' is not the last detected word")
-        else:
-            end_sample = int(last_word["start"] * 24000)
+        last_word_expected = end_of_sentence_words[-1]
+        last_word_generated = normalize_word(last_generated_words[-1]["text"])
+        if not last_word_expected in last_word_generated:
+            print(
+                f"Warning: Last word not in whisper output: {last_word_expected}, {last_word_generated}"
+            )
+
+        end_sample = int(last_generated_words[0]["start"] * 24000)
 
         fade_duration = 0.2
 
@@ -103,7 +95,7 @@ class SentenceTTS:
         # Apply the fade to the last `fade_samples` of the audio
         audio_tensor = torch.tensor(out["wav"])
         fade_start = end_sample - fade_samples
-        audio_tensor[fade_start : int(end_sample)] *= fade_out_envelope
+        audio_tensor[fade_start:end_sample] *= fade_out_envelope
 
         output = audio_tensor[: end_sample + 1]
         # Ensure the audio ends with zero amplitude to avoid clicking noise
